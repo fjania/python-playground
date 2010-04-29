e988024e56a6d7f11dc1e9b37136ca76fc27710f
Frank Jania <fjania@gmail.com>
2010-04-28 21:19:42 -0400

    more fibonacci updates

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -2,32 +2,21 @@ def memoize(f):
 	memoized = {}
 	def wrapped(arg):
 		if memoized.get(arg):
-			print "cache hit:", arg
+			# print "cache hit  => memoized[%s]: %s" %(arg, memoized.get(arg))
 			return memoized.get(arg)
 		else:
-			print "cache MISS:" , arg
 			memoized[arg] = f(arg)
+			print "cache MISS => memoized[%s]: %s" %(arg, memoized.get(arg))
 			return f(arg)
 	return wrapped
 	
-
-@memoize
-def do_something(text):
-	# print text
-	return text
-
-do_something("hello")
-do_something("hello")
-do_something("hello")
-do_something("nothing")
-do_something("hello")
 
 @memoize
 def fibonacci_memoized(n):
   if n in (0, 1): return n
   return fibonacci_memoized(n - 1) + fibonacci_memoized(n - 2)
 
-print "f(10):" , fibonacci_memoized(10)
+fibonacci_memoized(100)
 
 
 def dfwa(a1, a2):
